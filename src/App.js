c8ca2d1ccfc18a2d2d022b7c96d71164b3d11b4f
Samuel <73571779+mugetsu0422@users.noreply.github.com>
2023-10-28 08:37:01 +0700

    start
    add search bar
    add api call to get image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
 
-function App() {
+function SearchBar({ searchText, setSearchText, searchClicked }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="searchField">
+        <label htmlFor="iname"></label>
+        <input type="text" id="iname" name="iname" value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+        <button onClick={searchClicked}>Search</button>
+      </div>
+    </>
   );
 }
 
-export default App;
+function Gallery({ imageURLs }) {
+  return (
+    <>
+      {imageURLs.map((val, idx) => {
+        return <img key={idx} src={val} alt={''} width={300}></img>;
+      })}
+    </>
+  );
+}
+
+function ImageSearch() {
+  const [searchText, setSearchText] = useState('')
+  const [finalSearchText, setFinalSearchText] = useState('')
+  const [imageURLs, setImageURLs] = useState([]);
+
+  async function loadImages(search) {
+    const key = 'kZvumdGEtWnKotZ_OWWwcNNCHIapi7kCOQsoTISm4sc'
+    const res = await fetch(
+      `https://api.unsplash.com/search/photos?client_id=${key}&query=${search}&per_page=100`
+    );
+    const imageResult = await res.json();
+    setImageURLs(imageResult.results.map((image) => image.urls.small));
+  }
+  
+  useEffect(() => {
+    loadImages(finalSearchText);
+  }, [finalSearchText]);
+
+  function searchClicked() {
+    setFinalSearchText(searchText)
+  }
+
+  return (
+    <>
+      <SearchBar searchText={searchText} setSearchText={setSearchText} searchClicked={searchClicked}></SearchBar>
+      <Gallery imageURLs={imageURLs}></Gallery>
+    </>
+  );
+}
+
+export default ImageSearch;
